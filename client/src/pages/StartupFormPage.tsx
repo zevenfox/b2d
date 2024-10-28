6e28c05d7208a6d38398d93bff5ebbafbb0b3d08
Sittanat <apalakawongna@gmail.com>
2024-10-28 11:58:36 +0700

    add back button

--- client/src/pages/StartupFormPage.tsx
+++ client/src/pages/StartupFormPage.tsx
@@ -168,6 +168,13 @@ const StartupSignUp: React.FC = () => {
 
     return (
         <div className="min-h-screen bg-gray-100 flex flex-col justify-center py-12 sm:px-6 lg:px-8">
+            <button
+                type="button"
+                onClick={() => navigate('/index')}
+                className="absolute top-4 left-4 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+            >
+                Back
+            </button>
             <div className="sm:mx-auto sm:w-full sm:max-w-md">
                 <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
                     Sign Up as Startup
@@ -399,7 +406,7 @@ const StartupSignUp: React.FC = () => {
                         <div className="flex justify-between">
                             <button
                                 type="button"
-                                onClick={() => navigate('/')}
+                                onClick={() => navigate('/index')}
                                 className="w-full mr-2 py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 bg-white hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                             >
                                 Cancel
